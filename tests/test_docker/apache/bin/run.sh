750be5594a0027685b7d8052f262749b4abfb10f
Robin THONI <robin@rthoni.com>
2019-03-17 16:29:54 +0000

    updated test docker

--- tests/test_docker/apache/bin/run.sh
+++ tests/test_docker/apache/bin/run.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-gcf || exit 1
-
 cat /tmp/gcf-model.json | jq -r '.model.apache2.vhosts[] | @base64' | while read vhost; do
     vhost_name="$(echo "${vhost}" | base64 -d | jq -r .name)"
     echo "Serving host: ${vhost_name}"
